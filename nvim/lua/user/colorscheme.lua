22a99b6f682f237056a52887f470be8a734ba5e3
drakeqiu <adidasqiu@gmail.com>
2023-06-16 06:41:56 +0800

    feat: clear old settings

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -3,4 +3,4 @@ vim.opt.termguicolors = true
 -- vim.g.gruvbox_material_background = "hard"
 -- vim.g.gruvbox_material_better_performance = 1
 -- vim.cmd([[colorscheme gruvbox-material]])
-vim.cmd([[colorscheme gruvbox]])
+--vim.cmd([[colorscheme gruvbox]])
